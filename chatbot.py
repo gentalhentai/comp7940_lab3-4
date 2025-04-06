372bc7502331ade4e9833b228387ff52c9dce6cb
gentalhentai <49157069+gentalhentai@users.noreply.github.com>
2025-04-07 00:06:25 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import os
 from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,CallbackContext)
-#import configparser
+import configparser
 import logging
 import redis
 
@@ -16,11 +16,11 @@ def equiped_chatgpt(update, context):
 global redis1
 def main():
     # Load your token and create an Updater for your Bot
-    # config = configparser.ConfigParser()
-    # config.read('config.ini')
+    config = configparser.ConfigParser()
+    config.read('config.ini')
     
-    # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
-    updater = Updater(token=(os.environ[KEY_WORDS]), use_context=True)
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # updater = Updater(token=(os.environ[KEY_WORDS]), use_context=True)
     dispatcher = updater.dispatcher
     global redis1
     redis1 = redis.Redis(host=(config['REDIS']['HOST']), 
