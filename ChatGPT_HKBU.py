3fc555f6be51e275c8a3f82f67fb064cf1755182
gentalhentai <49157069+gentalhentai@users.noreply.github.com>
2025-04-07 16:42:40 +0800

    Update ChatGPT_HKBU.py

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -1,28 +1,40 @@
-import configparser
+# import configparser
+import os
 import requests
+
+
 class HKBU_ChatGPT():
-    def __init__(self,config_='./config.ini'):
-        if type(config_) == str:
-            self.config = configparser.ConfigParser()
-            self.config.read(config_)
-        elif type(config_) == configparser.ConfigParser:
-            self.config = config_
-    def submit(self,message):   
+    def __init__(self):
+        # if type(config_) == str:
+        #     # self.config = configparser.ConfigParser()
+        #     # self.config.read(config_)
+        # elif type(config_) == configparser.ConfigParser:
+        #     self.config = config_
+        self.basicurl = os.environ['BASICURL']
+        self.modelname = os.environ['MODELNAME']
+        self.verison = os.environ['APIVERSION']
+        self.apikey = os.environ['GPT_ACCESS_TOKEN']
+
+    def submit(self, message):
         conversation = [{"role": "user", "content": message}]
-        
-        url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
-        
-        headers = { 'Content-Type': 'application/json','api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
-        payload = { 'messages': conversation }
+
+        url = (self.basicurl + "/deployments/" + self.modelname
+               + "/chat/completions/?api-version=" + self.verison)
+
+        headers = {'Content-Type': 'application/json',
+                   'api-key': self.apikey}
+        payload = {'messages': conversation}
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
             data = response.json()
             return data['choices'][0]['message']['content']
         else:
             return 'Error:', response
+
+
 if __name__ == '__main__':
     ChatGPT_test = HKBU_ChatGPT()
-    while True:   
+    while True:
         user_input = input("Typing anything to ChatGPT:\t")
         response = ChatGPT_test.submit(user_input)
         print(response)
